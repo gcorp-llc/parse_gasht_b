103de639cbd036568ac9a0abe73f5328c56c5800
GCORP LLC <hosseinradvictor@gmail.com>
2024-10-30 20:49:21 +0330

    Update User Api Routering Style

--- routes/api.php
+++ routes/api.php
@@ -6,4 +6,15 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
 
 Route::apiResource('users', UserController::class);
+
+// OR
+
+// Route::get('users', [UserController::class, 'index'])->name('users.collection');
+// Route::post('users', [UserController::class, 'store'])->name('create.user');
+// Route::get('users//{id}', [UserController::class, 'show'])->name('show.user');
+// Route::put('users', [UserController::class, 'update'])->name('update.user');
+// Route::delete('users/{id}', [UserController::class, 'destroy'])->name('delete.user');
+
+
+
 Route::get('countries',[CountryController::class,'index']);
